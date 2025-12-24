74e0bab8f47a39f7a711e60f7c5dc322ce0c4618
matan2288 <matan2288@gmail.com>
2025-12-24 15:33:42 +0200

    Finsihed exercise 4

--- Exercises/Qualigrade/Exercise4/Time.java
+++ Exercises/Qualigrade/Exercise4/Time.java
@@ -1,25 +1,182 @@
-// They want you to implement a Time class that represents the time of 
-// day as seconds since midnight (0–86399), ensuring the value always stays within that range.
 
-// midnight 0, next day midnight 86400
 public class Time {
-    private int finalSeconds; // validate if it is between 0-86399
+    private int seconds;
 
-    public int CalculateSeconds(int seconds, int minutes, int hours) {
-        seconds = (seconds % 86400 + 86400) % 86400;
-        minutes = minutes * 60; // display in seconds
-        hours = hours * 3600; // display in seconds
-        System.out.println("Seconds: " + seconds + " seconds");
-        System.out.println("Minutes: " + minutes + " seconds");
-        System.out.println("Hours: " + hours + " seconds");
-
-        int res = seconds + minutes + hours;
-        return seconds + minutes + hours;
+    // ? Question 1
+    public int getValidatedSecondsRange(int seconds) {
+        return (seconds % 86400 + 86400) % 86400;
     }
 
-    void main() {
-        CalculateSeconds(5, 60, 2);
+    // Empty Constructor
+    public Time() {
+
+    }
+
+    public Time(int hours, int minutes, int seconds) {
+        int convertedSeconds = (seconds % 86400 + 86400) % 86400;
+        int convertedMinutesToSeconds = minutes * 60;
+        int convertedHoursToSeconds = (hours * 3600);
+        int totalSeconds = convertedSeconds + convertedMinutesToSeconds + convertedHoursToSeconds;
+        this.seconds = getValidatedSecondsRange(totalSeconds);
+    }
+
+    //////////////////////////////////////////////////////////////
+
+    // ? Question 2
+    private int reminder;
+
+    public int getHour() {
+        int reminderFromHours = seconds % 3600;
+        reminder = reminderFromHours;
+
+        int exactHourInSeconds = seconds - reminder;
+        int exactHourConvertedFromSeconds = exactHourInSeconds / 3600;
+
+        return exactHourConvertedFromSeconds;
+    }
+
+    public int getMinute() {
+        int reminderFromMinutes = reminder % 60;
+        int excatMinuteInSeconds = reminder - reminderFromMinutes;
+
+        reminder = reminderFromMinutes;
+
+        int exacMinuteConvertedFromSeconds = excatMinuteInSeconds / 60;
+
+        return exacMinuteConvertedFromSeconds;
+    }
+
+    public int getSecond() {
+        return reminder;
+    }
+
+    //////////////////////////////////////////////////////////////
+
+    // ? Question 3
+    public void add(int hours, int minutes, int passedSeconds) {
+        int hoursInSeconds = (hours * 3600 % 86400 + 86400) % 86400;
+        int minutesInSeconds = (minutes * 60 % 86400 + 86400) % 86400;
+        int totalInSecondsToAdd = hoursInSeconds + minutesInSeconds + passedSeconds;
+
+        seconds = (seconds + totalInSecondsToAdd) % 86400;
+    }
+
+    //////////////////////////////////////////////////////////////
+
+    // ? Question 4
+    public double getDays() {
+        double secondsInDouble = this.seconds;
+        return secondsInDouble / 86400;
+    }
+
+    //////////////////////////////////////////////////////////////
+
+    // ? Question 5
+    static String validateNum(int num) {
+        if (num < 10 || num == 0 || num == 00) {
+            return "0" + String.valueOf(num);
+
+        }
+        return String.valueOf(num);
+    }
+
+    @Override
+    public String toString() {
+        int h = this.getHour();
+        int m = this.getMinute();
+        int s = this.getSecond();
+
+        String timeInString = String.valueOf(h) + ":" + this.validateNum(m) + ":" + this.validateNum(s);
+
+        return timeInString;
+    }
+    //////////////////////////////////////////////////////////////
+
+    // ? Question 6
+    static class AccurateTime extends Time {
+        private int milliseconds;
+
+        public int getMilliseconds() {
+            return milliseconds;
+        }
+
+        public void setMilliseconds(int ms) {
+            if (ms > 999) {
+                double totalSecondsInMiliseconds = (int) ms / 1000;
+                int leftoverSeconds = (int) totalSecondsInMiliseconds;
+                int leftoverMiliseconds = (int) ((totalSecondsInMiliseconds - leftoverSeconds) * 1000);
+
+                milliseconds = leftoverMiliseconds;
+
+            } else if (ms <= 0) {
+                milliseconds = 0;
+
+            } else {
+                milliseconds = ms;
+            }
+        }
+
+        // Constructor with 3 parameter(assumes the number of milliseconds is zero)
+        public AccurateTime(int h, int m, int s) {
+            super(h, m, s);
+            setMilliseconds(0);
+            getMilliseconds();
+        }
+
+        // Constructor with 4 paramaters(4th parameter is the real milliseconds)
+        // public AccurateTime(int h, int m, int s, int ms) {
+        // super(h, m, s);
+        // setMilliseconds(ms);
+        // getMilliseconds();
+        // }
+
+        // ? Question 7
+        @Override
+        public String toString() {
+            String convertedMsToString;
+
+            if (getMilliseconds() == 0) {
+                convertedMsToString = "000";
+            } else {
+                convertedMsToString = String.valueOf(getMilliseconds());
+            }
+
+            String timeInString = super.toString() + "." + convertedMsToString;
+
+            return timeInString;
+        }
+
+        public AccurateTime(int h, int m, int s, int ms) {
+            super(h, m, s);
+            setMilliseconds(ms);
+            getMilliseconds();
+
+        }
+
+        // ? Question 8
+        @Override
+        public double getDays() {
+            return (super.seconds + getMilliseconds() / 1000.0) / 86400.0;
+        }
+
+        public static void main(String[] args) {
+            AccurateTime t1 = new AccurateTime(10, 30, 0, 0);
+            AccurateTime t2 = new AccurateTime(10, 30, 0, 432);
+
+            double n = t2.getDays();
+            System.out.println(n);
+        }
+
+    }
+    //////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        Time now = new Time(6, 45, 0);
+        System.out.println(now);
+        // ? Question 5
+
+        // String s = now.toString();
     }
 }
-// Time now = new Time(0,0,37800); // 37800 seconds past midnight
-// Time now = new Time(-14,20,600);
